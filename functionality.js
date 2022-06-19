3677fc51ad00e17ee1eb34356a197fcc318639e7
alostracco <106183667+alostracco@users.noreply.github.com>
2022-06-18 20:33:58 -0400

    algo updates

--- functionality.js
+++ functionality.js
@@ -3,9 +3,14 @@ function startStopButton() {
     if (button.innerHTML == "Start") {
         button.innerHTML = "Stop";
         var title = document.getElementById("algoTitle").innerHTML;
+        var barChart = document.getElementById("barChart");
+        var barsList = barChart.childNodes;
+        var endIndex = barsList.length;
         if (title == "Bubble Sort") bubble();
         else if (title == "Insertion Sort") insertion();
         else if (title == "Selection Sort") selection();
+        else if (title == "Merge Sort") merge(0, endIndex);
+        else if (title == "Quick Sort") quick(0, endIndex-1);
     }
     else {
         button.innerHTML = "Start";
--- sortAlgos.js
+++ sortAlgos.js
@@ -1,16 +1,13 @@
 const delay = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
 
 async function bubble() {
+    
+
     var speed = algoSpeed();
     var barChart = document.getElementById("barChart");
     var barsList = barChart.childNodes;
     var sorted = false;
     while (!sorted) {
-        /*
-        var bob = document.getElementById("bob");
-        var divElement = document.createElement("p");
-        divElement.innerHTML = "yes";
-        bob.appendChild(divElement); */
         sorted = true;
         for (var i = 0; i < barsList.length-1; i++) {
             var curBar = barsList[i];
@@ -68,8 +65,77 @@ async function selection() {
     }
 }
 
-async function heap() {
+async function mergeCombine(left, right, isSameLength) {
     var speed = algoSpeed();
     var barChart = document.getElementById("barChart");
     var barsList = barChart.childNodes;
+
+    var leftSize = right - left;
+    if (isSameLength) var rightSize = leftSize;
+    else var rightSize = leftSize + 1;
+    
+    while (leftSize != 0 && rightSize != 0) {
+        if (Number(barsList[left].innerHTML) < Number(barsList[right].innerHTML)) {
+            //barsList[left].parentNode.insertBefore(barsList[left], barsList[left+1]);
+            left++;
+            leftSize--;
+        } else {
+            barsList[right].parentNode.insertBefore(barsList[right], barsList[left]);
+            right++;
+            left++;
+            rightSize--;
+        }
+    }
+}
+
+function merge(startIndex, endIndex) {
+    if (endIndex-startIndex <= 1) return startIndex;
+
+    var midPoint = Math.floor((endIndex-startIndex) / 2 + startIndex);
+    var isSameLength = false;
+    if ((endIndex-startIndex) % 2 == 0) isSameLength = true;
+
+    mergeCombine(merge(startIndex, midPoint), merge(midPoint, endIndex), isSameLength);
+    return startIndex;
+}
+
+
+function quickCombine(left, right) {
+    var speed = algoSpeed();
+    var barChart = document.getElementById("barChart");
+    var barsList = barChart.childNodes;
+
+    var midPoint = Math.floor((right-left) / 2 + left);
+    barsList[midPoint].parentNode.insertBefore(barsList[midPoint], barsList[left]);
+    barsList[left+1].parentNode.insertBefore(barsList[left+1], barsList[midPoint+1]);
+
+    var pivot = Number(barsList[left].textContent);
+    var low = left + 1;
+    var high = right;
+
+    while (low <= high) {
+        while (Number(barsList[high].textContent) > pivot) high--;
+        while (low <= high && Number(barsList[low].textContent) <= pivot) low++;
+        if (low <= high) {
+            barsList[high].parentNode.insertBefore(barsList[high], barsList[low]);
+            barsList[low+1].parentNode.insertBefore(barsList[low+1], barsList[high+1]);
+            low++;
+            high--;
+        }
+    }
+    barsList[high].parentNode.insertBefore(barsList[high], barsList[left]);
+    barsList[left+1].parentNode.insertBefore(barsList[left+1], barsList[high+1]);
+
+    return high;
+}
+
+function quick(left, right) {
+    if (left >= right) return;
+    var partition = quickCombine(left, right);
+    quick(left, partition-1);
+    quick(partition+1, right);
+}
+
+function heap() {
+    
 }
